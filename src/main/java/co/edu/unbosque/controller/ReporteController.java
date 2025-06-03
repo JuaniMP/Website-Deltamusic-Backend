8cf52eb380b0032fa85de13489d175bbcf5c0705
Juanita Mejia <juamejiapi@gmail.com>
2025-06-03 05:59:30 -0500

    uwuw

--- src/main/java/co/edu/unbosque/controller/ReporteController.java
+++ src/main/java/co/edu/unbosque/controller/ReporteController.java
@@ -1,6 +1,8 @@
 package co.edu.unbosque.controller;
 
 import co.edu.unbosque.utils.ReporteService;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
@@ -8,6 +10,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+@CrossOrigin(origins = "*")
+@Slf4j
 @RestController
 @RequestMapping("/reporte")
 public class ReporteController {
